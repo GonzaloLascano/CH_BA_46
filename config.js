206a07d1a9077efccaec1bf618fa15e5791bd3c3
Gonzalo Ivan Puniales Lascano <lascano.gonzalo.ivan@gmail.com>
2022-08-17 05:27:08 -0300

    Finalizado el desafio de la clase 30: Servidor Nginx

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.SERVER = {
-    PORT: process.env.PORT,
+    PORT: process.argv[2] || process.env.PORT || 3030, //este ultimo para poder distinguirlo mas facil del resto en caso de que algo falle.
 }
 
 config.MONGO = {
